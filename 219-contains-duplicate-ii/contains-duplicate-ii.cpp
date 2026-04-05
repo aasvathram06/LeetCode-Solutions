3325d0152f1054d55a5892da51bd848a1d5358fb
ASVATH RAM A <aasvathram06@gmail.com>
2026-04-05 08:35:12 +0530

    Time: 72 ms (68.59%) | Memory: 98.8 MB (31.01%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
-        map<int,int>hm;
-        for(int i=0;i<nums.size();i++) {
-            if(hm.find(nums[i])==hm.end()){
-                hm[nums[i]] =i;
-            }
-            else {
-                if(abs(i-hm[nums[i]])<=k){
-                  return true;
-                }
-                hm[nums[i]] =i;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+        if(mpp.find(nums[i])!=mpp.end()){
+            if(i-mpp[nums[i]]<=k){
+                return true;
             }
         }
+        mpp[nums[i]]=i;
+     }
         return false;
     }
 };
