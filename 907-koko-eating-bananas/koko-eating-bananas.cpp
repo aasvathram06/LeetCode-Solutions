6f9d37f1d6bb92404f4dd1e3388c1a1476acb701
ASVATH RAM A <aasvathram06@gmail.com>
2026-07-13 18:17:12 +0530

    Time: 24 ms (5.33%) | Memory: 23 MB (14.81%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findMax(vector<int>&piles){
+       int maxi=INT_MIN;
+       int n=piles.size();
+       for(int i =0;i<n;i++){
+        maxi=max(maxi,piles[i]);
+       }
+       return maxi;
+    }
+
+    int caltotalH(vector<int>&piles,int hourly){
+       int totalH=0;
+       int n=piles.size();
+       for(int i=0;i<n;i++){
+        totalH += ceil((double)piles[i]/(double)hourly);
+       }
+       return totalH;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=findMax(piles);
+        while(low<high){
+            int mid= (low+high) /2;
+            int totalH = caltotalH(piles,mid);
+            if(totalH <= h){
+                high =mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
